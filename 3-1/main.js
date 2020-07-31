c222870d8183ec55c13e4ef4308410e83810f529
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-07-31 16:23:48 +0900

    少し改良してみました。

--- 3-1/main.js
+++ 3-1/main.js
@@ -1,16 +1,6 @@
-const table = [
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  []
-]
+const table = [];
 for(let m = 0; m < 9 ; m++){
-  // table.push([]);
+  table.push([]);
   for(let n = 0; n < 9 ; n++){
     let kotae = (m + 1)*(n + 1);
     // console.log(kotae);
