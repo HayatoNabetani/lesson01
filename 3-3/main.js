77acd70539e3a0458b982e5b104c16c4055e43c2
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-07-31 17:29:59 +0900

    mとiの関係を考えるのが難しかったです

--- 3-3/main.js
+++ 3-3/main.js
@@ -2,8 +2,8 @@ const vertical = process.argv[2];
 
 for(let i = 0 , shape = ''; i < vertical ; i++){
 
-  for(let m = 0; m < vertical * 2 - 1; m++){
-    shape += m === 4 || m === 4 + i || m === 4 - i ? '*' : ' ';
+  for(let m = 0; m < vertical * 2 ; m++){
+    shape += 4-i <= m && m <= 4+i ? '*' : ' ';
   }
   console.log(shape);
   shape = '';
