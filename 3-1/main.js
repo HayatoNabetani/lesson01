52b2b324097c7188e4a486bdb742a4865648b837
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-07-31 16:22:51 +0900

    九九表を表示してみました。

---
+++ 3-1/main.js
@@ -0,0 +1,20 @@
+const table = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+]
+for(let m = 0; m < 9 ; m++){
+  // table.push([]);
+  for(let n = 0; n < 9 ; n++){
+    let kotae = (m + 1)*(n + 1);
+    // console.log(kotae);
+    table[m].push(kotae);
+  }
+}
+console.table(table);
