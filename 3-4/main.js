4afba3a95c97e88f78048b74edfac068e4cf20f7
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-02 19:18:32 +0900

    一応できました

--- 3-4/main.js
+++ 3-4/main.js
@@ -1,16 +1,24 @@
 const unum1 = process.argv[2].split('');
-// const unum2 = process.argv[3].split('');
+const unum2 = process.argv[3].split('');
 
 // console.log(unum1.split(''));
 // 一文字ずつ区切る
 let a;
 
+a = unum1[0];
+unum1[0] = unum1[unum1.length - 1];
+unum1[unum1.length - 1] = a;
 
-for(let i = 0; i < Math.floor(unum1.length / 2); i++){
-  console.log(Math.floor(unum1.length / 2));
-  a = unum1[i];
-  unum1[i] = unum1[unum1.length - 1 + i];
-  unum1[unum1.length - 1 + i] = a;
-}
 
-console.log(unum1);
+a = unum2[0];
+unum2[0] = unum2[unum2.length - 1];
+unum2[unum2.length - 1] = a;
+
+const num1 = Number(unum1.join(''));
+const num2 = Number(unum2.join(''));
+
+console.log(num1);
+console.log(num2);
+
+console.log(num1 + num2);
+
