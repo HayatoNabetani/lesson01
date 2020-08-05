60096b4a3737f6b905d4a531a4928005061e4bbf
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-05 23:17:46 +0900

    できました

---
+++ 4-2/main.js
@@ -0,0 +1,9 @@
+for(let i = 1; i <= 50 ; i++){
+  if (i % 3 === 0){
+    console.log("WOW");
+  } else if (String(i).includes('3')){
+    console.log("WOW");
+  } else {
+    console.log(i);
+  }
+}
