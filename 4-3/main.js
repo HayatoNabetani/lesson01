16d960deb344d5f45eca37fee13e47f6c958c8b2
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-05 23:50:06 +0900

    indexOf,substr,substringの使い方がわかりました。

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,10 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+
+let e = [];
+errorLogs.forEach(errorLog =>{
+  e = errorLog.split(':');
+  let Leng = (e[1].indexOf(' ',18));
+  console.log(e[1].substr(0,Leng));
+})
