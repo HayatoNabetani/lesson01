a3c1e89ca8c8b1b7abd3852a110907e4d276988f
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-07 10:07:16 +0900

    とりあえず並び替えまで終わっています

--- 5-2/main.js
+++ 5-2/main.js
@@ -1,22 +1,32 @@
-const url = 'users.json';
+const json = {
+  users: [
+    { name: "Suzuki", rank: "A", years: 3 },
+    { name: "Yamada", rank: "A", years: 5 },
+    { name: "Sato", rank: "B", years: 4 },
+    { name: "Ito", rank: "B", years: 7 },
+    { name: "Tanaka", rank: "C", years: 1 },
+    { name: "Takahashi", rank: "C", years: 10 },
+  ],
+};
 
-const getLocations = () => {
-  return new Promise(function(resolve){
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-
-    xhr.onreadystatechange=()=>{
-      if(xhr.readyState===4 && xhr.status === 200){
-        let result = JSON.parse(xhr.responseText);
-        resolve(result);
-      }
-    }
-  })
-}
-
-getLocations().then(function(a){
-  for(let i=0; i<a.users.length;i++){
-    console.log(a.users[i]);
+const a = [];
+for(let i = 0; i < json.users.length; i++){
+  if(json.users[i].rank === 'A' && json.users[i].years < 5){
+    a[i] = 3000 *  json.users[i].years +100000;
+  } else if (json.users[i].rank === 'A' && json.users[i].years >= 5){
+    a[i] = 3000 *  json.users[i].years +120000;
+  } else if (json.users[i].rank === 'B'){
+    a[i] = 4000 *  json.users[i].years +140000;
+  } else {
+    a[i] = 5000 *  json.users[i].years +160000;
   }
+}
+// 給与計算
+
+const b = a.sort((a,b) =>{
+  return b-a;
 })
+
+console.log(b);
+
+// 並び替え
