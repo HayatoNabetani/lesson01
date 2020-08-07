0164a9013338c6bbd1e28ef3d3e94176bd330632
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-07 11:00:04 +0900

    大きい順にソート

--- 5-3/main.js
+++ 5-3/main.js
@@ -1,8 +1,16 @@
 async function load(){
   const data = await fetch('playdata.json');
   const obj = await data.json();
+
+  const obj2 = obj.data.sort((a,b) =>{
+    return b.count - a.count;
+  })
+  // 大きい順に変える
   
-  console.log(obj);
-}
+  console.log(obj2);
+    
+};
+
+
 
 load();
