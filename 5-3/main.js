9f14a990713e6f6a29948ae79d7eeb6935f3b4ea
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-07 11:46:29 +0900

    if(objPlay2[i].id === objSong.songs[m].id)にすると出力されません。

--- 5-3/main.js
+++ 5-3/main.js
@@ -10,12 +10,20 @@ async function load(){
   const objPlay2 = objPlay.data.sort((a,b) =>{
     return b.count - a.count;
   })
-  // playdataを大きい順に変える
+  // playdataを大きい順に変える 
+  
 
-  console.log(objPlay2);
-    
+  for(let i = 0 ;i < objPlay2.length ; i++){   
+    for(let m = 0; m < objSong.songs.length ; m++){
+      // console.log(objPlay2[i].id);
+      // console.log(objSong.songs[m].id);
+      if(objPlay2[i].id == objSong.songs[m].id){
+        console.log(`順位:${objSong.songs[m].title}\t${objPlay2[i].count}`);
+        break;
+      }
+    }
+  }
+  //データを合わせる 
 };
-
-
 
 load();
