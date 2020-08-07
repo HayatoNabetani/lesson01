dfa1ed9152ee539da1951f7525f90d79c4edbd59
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-07 13:21:16 +0900

    MDNの力はすごい

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,12 +1,31 @@
-const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+const str = 'こはるはるここはるここはるはるここはるこはるはるはる';
 
+const myRe1 = /こはる/g;
+const myRe2 = /はるこ/g;
 
+let myArray1;
+// for Koharu
+let myArray2;
+// for Haruko
 
-const a = /こはる/.test(str);
-const b = /はるこ/.test(str);
+let countKoharu = 0;
+let countHaruko = 0;
 
-console.log(a);
-console.log(b);
+while ((myArray1 = myRe1.exec(str)) !== null) {
+  let msg = myArray1[0] + ' を見つけました。';
+  msg += '次の検索は ' + myRe1.lastIndex + ' からです。';
+  console.log(msg);
+  countKoharu++;
+}
+// coutKoharu
 
-// console.log(`こはる:${}`);
-// console.log(`はるか:${}`);
+while ((myArray2 = myRe2.exec(str)) !== null) {
+  let msg = myArray2[0] + ' を見つけました。';
+  msg += '次の検索は ' + myRe2.lastIndex + ' からです。';
+  console.log(msg);
+  countHaruko++;
+}
+// coutHaruko
+
+console.log(`こはる:${countKoharu}`);
+console.log(`はるこ:${countHaruko}`);
