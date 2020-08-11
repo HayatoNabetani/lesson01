60675359503cb6feb2f98d08584add3dd03db116
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 13:19:48 +0900

    とりあえず、残す

--- 4-8/main.js
+++ 4-8/main.js
@@ -4,22 +4,28 @@ let count = 0
 let index = 0
 
 for(;true;){
-  index = str.indexOf("こはる",index)
+  index = str.indexOf(process.argv[2],index)
+  //検索文字じゃない文字が来たらbreak;
   if(index === -1){
     break
+  }
+  //上のfor文が検索文字分回った場合
+  if(str.length > process.argv[2].length){
+    console.log("検索結果はありませんでした");
   }
   index++
   count++
 }
 console.log(count);
-// こはる
+
 
 
 count = 0
 index = 0
 
 for(;true;){
-  index = str.indexOf("はるこ",index)
+  index = str.indexOf(process.argv[3],index)
+  //検索文字じゃない文字が来たらbreak;
   if(index === -1){
     break
   }
@@ -28,4 +34,3 @@ for(;true;){
 }
 
 console.log(count);
-// はるこ
