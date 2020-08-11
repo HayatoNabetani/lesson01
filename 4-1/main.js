fecf1e361a4a5c42256e8e7083be1c1f9f80813f
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 11:42:39 +0900

    splice(2)の使い方それ以降の配列を新たに生成する

--- 4-1/main.js
+++ 4-1/main.js
@@ -1,11 +1,4 @@
-const nums = [
-  Number(process.argv[2]),
-  Number(process.argv[3]),
-  Number(process.argv[4]),
-  Number(process.argv[5]),
-  Number(process.argv[6]),
-]
-
+const nums = process.argv.splice(2)
 
 const newNum = nums.sort((a,b)=>{
   return a-b;
