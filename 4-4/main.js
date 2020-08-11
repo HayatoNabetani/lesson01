d47b7af78c58423643a0e4d82fc955e8c749bafa
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 11:56:42 +0900

    重複せずに作りました

--- 4-4/main.js
+++ 4-4/main.js
@@ -1,5 +1,21 @@
-const a = [];
-for(let i = 0 ;i < 10 ; i++){
-  a.push(Math.floor(Math.random()*9));
+/** 重複チェック用配列 */
+let randoms = [];
+/** 最小値と最大値 */
+const min = 0, max = 9;
+
+/** 重複チェックしながら乱数作成 */
+for(let i = min; i <= max; i++){
+  while(true){
+    var tmp = intRandom(min, max);
+    if(!randoms.includes(tmp)){
+      randoms.push(tmp);
+      break;
+    }
+  }
 }
-console.log(a);
+/** min以上max以下の整数値の乱数を返す */
+function intRandom(min, max){
+  return Math.floor( Math.random() * (max - min + 1)) + min;
+}
+
+console.log(randoms.join(' '));
