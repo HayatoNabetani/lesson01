5f6c9bf5d92c55140a515296f2e67c6f8ff356d7
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 11:32:33 +0900

    とりあえず、保存する

--- 3-5/main.js
+++ 3-5/main.js
@@ -6,18 +6,46 @@ const table = [
   ['','','','','','','','','','','']
 ]
 
+
+// どれだけ重複があるかを確認するために重複させる
+let c = [];
+students.forEach((student) =>{
+  c.push(Math.floor(student / 10));
+})
+console.log(c);
+
+// 重複の個数を調べる
+let counts = [];
+for (let i = 0; i < c.length; i++) {
+  let key = c[i];
+  counts[key] = (counts[key]) ? counts[key] + 1 : 1;
+}
+// const array = [];
+// array.push(counts);
+// console.log(array);
+
+// console.log(Math.max(...arr.map((p) => p.age)))
+
+// console.log(counts.reduce((a,b)=>Math.max(a,b)));
+// 配列の最大値はこれ
+
+// 最大のものを見る
+const max = counts.reduce((a,b)=>Math.max(a,b));
+
+// 星をいれる
 students.forEach((student) =>{
   let i = Math.floor(student / 10);
-  if (table[2][i] === ''){
-    table[2][i] = '*';
+  if (table[max-1][i] === ''){
+    table[max-1][i] = '*';
   } else if(table[1][i] === ''){
-    table[1][i] = '*';
+    table[max-2][i] = '*';
   } else {
-    table[0][i] = '*';
+    table[max-3][i] = '*';
   }
 })
 
-for(let i = 0 ;i < 3 ; i++){
+// 空白をいれる
+for(let i = 0 ;i < max ; i++){
   for(let m = 0 ;m < 11 ; m++){
     if(table[i][m]===''){
       table[i][m] = ' ';
