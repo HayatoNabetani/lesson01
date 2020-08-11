00e043390c1181be512824e936944d624b761661
nabeatu <nabe_baseball1111@yahoo.co.jp>
2020-08-11 11:38:16 +0900

    これ配列出力してまたそれを繰り返しは難しいです

--- 3-5/main.js
+++ 3-5/main.js
@@ -1,18 +1,10 @@
 const students = [17,38,100,95,23,62,77,45,69,81,83,51,42,36,60]
 
-const table = [
-  ['','','','','','','','','','',''],
-  ['','','','','','','','','','',''],
-  ['','','','','','','','','','','']
-]
-
-
 // どれだけ重複があるかを確認するために重複させる
 let c = [];
 students.forEach((student) =>{
   c.push(Math.floor(student / 10));
 })
-console.log(c);
 
 // 重複の個数を調べる
 let counts = [];
@@ -20,17 +12,19 @@ for (let i = 0; i < c.length; i++) {
   let key = c[i];
   counts[key] = (counts[key]) ? counts[key] + 1 : 1;
 }
-// const array = [];
-// array.push(counts);
-// console.log(array);
-
-// console.log(Math.max(...arr.map((p) => p.age)))
 
 // console.log(counts.reduce((a,b)=>Math.max(a,b)));
 // 配列の最大値はこれ
 
 // 最大のものを見る
 const max = counts.reduce((a,b)=>Math.max(a,b));
+
+
+// 星を入れる箱を準備する
+const table = [];
+for(let i = 0; i < max ; i++){
+  table.push(['','','','','','','','','','',''])
+}
 
 // 星をいれる
 students.forEach((student) =>{
@@ -52,11 +46,6 @@ for(let i = 0 ;i < max ; i++){
     }
   }
 }
-
-
-// console.table(table[0]);
-// console.table(table[1]);
-// console.table(table[2]);
 
 console.log(table[0].join('  '));
 console.log(table[1].join('  '));
